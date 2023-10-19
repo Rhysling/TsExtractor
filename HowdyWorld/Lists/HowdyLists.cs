3eb4d2234eb43f81a977d3db4223212f2d046d55
Rhysling <rkummer@polson.com>
2023-10-19 09:00:16 -0700

    Update packages.

---
+++ HowdyWorld/Lists/HowdyLists.cs
@@ -0,0 +1,11 @@
+﻿using HowdyWorld.Models;
+using System.Collections.Generic;
+
+namespace HowdyWorld.Lists
+{
+	[TypeScriptModel]
+		public class HowdyLists
+		{
+				public List<HowdyVM> ListOfVms { get; set; }
+		}
+}
